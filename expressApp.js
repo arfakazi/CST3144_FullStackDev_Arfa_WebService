d0ccc3e3849aad8c6a4c837df05966d0cbaba694
Arfa Kazi <arfakazi@gmail.com>
2024-12-01 00:15:00 +0400

    Express and Mongodb Setup

--- expressApp.js
+++ expressApp.js
@@ -1,1 +1,27 @@
-// install express, node, mongodb
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+
+const app = express();
+app.use(express.json());
+app.set("port", 3000);
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    next();
+});
+
+let db;
+MongoClient.connect(
+    "mongodb+srv://arfakazi:jimin1310@cluster0.jpux4ux.mongodb.net/",
+    (err, client) => {
+        db = client.db("Aras_Classes");
+    }
+);
+
+// Routing

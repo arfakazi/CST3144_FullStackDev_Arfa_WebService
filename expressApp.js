98b0055d0bf01ea71248f824e4609c57723ae537
Arfa Kazi <arfakazi@gmail.com>
2024-12-03 17:07:13 +0400

    Modified expressApp.js

--- expressApp.js
+++ expressApp.js
@@ -25,3 +25,62 @@ MongoClient.connect(
 );
 
 // Routing
+
+app.get("/", (req, res, next) => {
+    res.send("select a collection,e.g.,/collection/messages");
+});
+
+// Sets the collection Name
+app.param("collectionName", (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+//
+app.get("/collection/:collectionName", (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
+        res.send(results);
+    });
+});
+
+// ??
+app.post("/collection/:collectionName", (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e);
+        res.send(results.ops);
+    });
+});
+
+const ObjectID = require("mongodb").ObjectID;
+app.get("/collection/:collectionName/:id", (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+        if (e) return next(e);
+        res.send(result);
+    });
+});
+
+// Update an object
+app.put("/collection/:collectionName/:id", (req, res, next) => {
+    req.collection.update(
+        { _id: new ObjectID(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e) return next(e);
+            res.send(result.result.n === 1 ? { msg: "Success :D" } : { msg: "Error :(" });
+        }
+    );
+});
+
+// Delete an object
+app.delete("/collection/:collectionName/:id", (req, res, next) => {
+    req.collection.deleteOne({ _id: ObjectID(req.params.id) }, (e, result) => {
+        if (e) return next(e);
+        res.send(result.result.n === 1 ? { msg: "Success!!" } : { msg: "error" });
+    });
+});
+
+app.listen(3000, () => {
+    console.log("express.js server running at localhost:3000");
+});

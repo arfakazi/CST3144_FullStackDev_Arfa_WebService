355dfffb449799a576aadb527c5ce46de993f3cb
Arfa Kazi <arfakazi@gmail.com>
2024-12-03 17:27:26 +0400

    Added REST API routes

--- expressApp.js
+++ expressApp.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require("mongodb").ObjectID;
-
 const app = express();
-app.use(express.json());
-app.set("port", 3000);
-app.use((req, res, next) => {
+const ObjectID = require("mongodb").ObjectID;
+const MongoClient = require("mongodb").MongoClient;
+const path = require("path");
+
+function config(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
@@ -14,70 +13,88 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     );
     next();
-});
+}
+
+app.use(express.json());
+app.set("port", 3000);
+app.use(config);
 
 let db;
 MongoClient.connect(process.env.CONNECTION_STRING, (err, client) => {
+    if (err) throw err;
     db = client.db(process.env.DATABASE);
 });
 
-// Routing
-
-app.get("/", (req, res, next) => {
-    res.send("select a collection,e.g.,/collection/messages");
+app.get("/", (res, req, next) => {
+    console.log("Accessed root endpoint");
+    res.send("Select a collection, e.g., /collection/messages");
 });
 
-// Sets the collection Name
-app.param("collectionName", (req, res, next, collectionName) => {
+app.param("collectionName", (res, req, next) => {
+    console.log(`Collection set to: ${collectionName}`);
     req.collection = db.collection(collectionName);
     return next();
 });
 
-//
-app.get("/collection/:collectionName", (req, res, next) => {
+app.get("/collection/:collectionName", (res, req, next) => {
+    console.log(`Retrieving objects from collection: ${req.params.collectionName}`);
     req.collection.find({}).toArray((e, results) => {
         if (e) return next(e);
         res.send(results);
     });
 });
 
-// ??
-app.post("/collection/:collectionName", (req, res, next) => {
+app.post("/collection/:collectionName", (res, req, next) => {
+    console.log(`Adding an object to collection: ${req.params.collectionName}`);
     req.collection.insert(req.body, (e, results) => {
         if (e) return next(e);
+        console.log("Object added:", results.ops);
         res.send(results.ops);
     });
 });
 
-const ObjectID = require("mongodb").ObjectID;
-app.get("/collection/:collectionName/:id", (req, res, next) => {
-    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
-        if (e) return next(e);
-        res.send(result);
-    });
-});
-
-// Update an object
-app.put("/collection/:collectionName/:id", (req, res, next) => {
+app.put("/collection/:collectionName/:id", (res, req, next) => {
+    console.log(
+        `Updating object in collection: ${req.params.collectionName} with ID: ${req.params.id}`
+    );
     req.collection.update(
         { _id: new ObjectID(req.params.id) },
         { $set: req.body },
         { safe: true, multi: false },
         (e, result) => {
             if (e) return next(e);
-            res.send(result.result.n === 1 ? { msg: "Success :D" } : { msg: "Error :(" });
+            console.log(
+                result.result.n === 1 ? "Object updated successfully" : "Object update failed"
+            );
+            res.send(result.result.n === 1 ? { msg: "success" } : { msg: "error" });
         }
     );
 });
 
-// Delete an object
-app.delete("/collection/:collectionName/:id", (req, res, next) => {
-    req.collection.deleteOne({ _id: ObjectID(req.params.id) }, (e, result) => {
-        if (e) return next(e);
-        res.send(result.result.n === 1 ? { msg: "Success!!" } : { msg: "error" });
-    });
+app.get("/search/:collectionName", (res, req, next) => {
+    const searchQuery = req.query.query || "";
+    console.log(`Searching in collection: ${req.params.collectionName} with query: ${searchQuery}`);
+    const searchRegex = new RegExp(searchQuery, "i");
+    req.collection
+        .find({
+            $or: [
+                { subject: searchRegex },
+                { location: searchRegex },
+                { price: { $regex: searchRegex } },
+                { spaces: { $regex: searchRegex } },
+            ],
+        })
+        .toArray((e, results) => {
+            if (e) return next(e);
+            console.log(`Search results: ${results.length} documents found`);
+            res.send(results);
+        });
 });
 
-app.listen(3000, () => {
-    console.log("express.js server running at localhost:3000");
+var imagePath = path.resolve(__dirname, "static");
+app.use("/static", express.static(imagePath));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Express.js server running at localhost:" + port);
 });
